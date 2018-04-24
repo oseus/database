0ec08e7b84a1358bfd2e2e95a00d65031767c75f
misterd <coat.david@gmail.com>
2018-04-25 01:27:33 +0200

    funny time

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# database
+## README THP_CODE4#LYON
+
+Hello et bienvenue sur githoub, excellente correction !
+
+Theme de jour : Faire une database avec Rails et ActiveRecord
+
+
+
+## Githoub des peer-learning :
+
+* Mohammad-Ali
+* Odssey
+* oseus
+* Hug-O
+
+## Slack THP des contributeurs :
+
+* Mohammad-Ali : @Mohammad-Ali
+* Odyssée Levine : @Odyssée Levine
+* David Coat : @Coat David
+* Hugo : @Hugo
+
+
+## Infos importantes sur le programme
+
+##Exercice 1 : faire une database
+
+Alors pour bien voir le résultat de cet exercice je vous conseil d'utiliser "DB Browser for SQLITE" et ouvrir le fichier "development.sqlite3" qui se trouve dans le dossier "db".
+
+N'hésitez pas à voir les fichers Ruby du dossier "migrate" qui explique pas à pas ma démarche pour tout créer et lier les Tables (oui oui les tables le sont la preuve est l'apparition des colonnes id grâce à la ligne de de commande "has_many/belongs_to".)
+
+##Exercice 2 : remplissage
+
+Pour celui-ci il faut que vous regardiez le fichier Ruby "Seed.rb" qui montre la démarche (bon un peu moins pas à pas mais il peut être comique à lire pour certains^^).
+Bien que les tables soient liés, je n'ai pas réussi à lier les éléments dans les tables (et ça malgré la super commande du tutoriel de THP.... mais bon si je suis le seul pour lequel ça a foiré j'ai hâte que tu m'explique comment faire ;) )
+
+Ah j'oubliais, les premiers qui sont links(bien que pas sûre que ça vous les affiches) ce sont des faits maisons, ou plutôt faits souris c'est pour que ceux qui veulent jouer avec les lignes de commandes voient que les Tables sont vraiments liées (et aussi pour montrer que c'est possible de remplir les colonnes "_id" à la main mais comme j'ai dit pas sûre que ça vous les affiches de toutes façon, vous me direz demain^^)
+
+##Exercice 3 : l'heure contre la montre
+
+Il n'était pas optionnel? oups... et si on disait qu'il l'était ? Je suis sûre que ça arrangerait beaucoup de monde^^. 
+
+Oh et promis le prochain Readme sera en anglais ;).
--- test-activerecords-OKLM/db/seeds.rb
+++ test-activerecords-OKLM/db/seeds.rb
@@ -22,3 +22,80 @@ end
 200.times do
  categories = Category.create(name: Faker::Commerce.department(2, true))
 end
+
+#Here I'm creating 10 users with 10 articles and 10 categories
+
+user1 = User.create(name: 'Jabba the Hutt')
+articles1 = Article.create(name: "Born to be a larva", body: "Sorry but I not gonna do a full body for you XD", description: "Slim of slug")
+categories1 = Category.create(name: "Dodo")
+
+user2 = User.create(name: 'Choubaka Chewi')
+articles2 = Article.create(name: "hoiuhioh qlsdhpoa", body: "Sorry but I not gonna do a full body for you XD", description: "kjbdqsl amodnmandz")
+categories2 = Category.create(name: "Riri")
+
+user3 = User.create(name: 'Yoda dada')
+articles3 = Article.create(name: "How to speak like a boss", body: "Sorry but I not gonna do a full body for you XD", description: " green little creature")
+categories3 = Category.create(name: "fifi")
+
+user4 = User.create(name: 'Darth Sidious')
+articles4 = Article.create(name: "How to rule a universe in ten lessons", body: "Sorry but I not gonna do a full body for you XD", description: "Unlimitted POWEEEEEEER")
+categories4 = Category.create(name: "loulou" )
+
+user5 = User.create(name: 'Luke Skywalker')
+articles5 = Article.create(name: "How to don't fall in love with your sister", body: "Sorry but I not gonna do a full body for you XD", description: "I'm a bad teacher")
+categories5 = Category.create(name: "Tuture")
+
+user6 = User.create(name: 'Obi-Wan Kenobi')
+articles6 = Article.create(name: "How to disappear in 10 lessons", body: "Sorry but I not gonna do a full body for you XD", description: "You see me, you don't see me anymore, you see me a little, you see me ,you don't see me XD")
+categories6 = Category.create(name: "magic finger" )
+
+user7 = User.create(name: 'Leila Organa')
+articles7 = Article.create(name: "How to create a real Republic", body: "Sorry but I not gonna do a full body for you XD", description: "I will never die")
+categories7 = Category.create(name: " bobibu" )
+
+user8 = User.create(name: 'Boba Fet')
+articles8 = Article.create(name: "How to be a good Booty Hunter", body: "Sorry but I not gonna do a full body for you XD", description: "yololololo")
+categories8 = Category.create(name: "Testosterone")
+
+user9 = User.create(name: 'Jarjar Bings')
+articles9 = Article.create(name: "How to kill a Republic...for the noob", body: "Sorry but I not gonna do a full body for you XD", description: "Misa-misa")
+categories9 = Category.create(name: "Toto")
+
+user10 = User.create(name: 'Darth Vader')
+articles10 = Article.create(name: "The Dark Side", body: "Sorry but I not gonna do a full body for you XD", description: "You are sure that it's not your father?")
+categories10 = Category.create(name: "Tata")
+
+#Here I'm linking each user with his article and his category
+
+user1 = articles1
+articles1 = categories1
+
+user2 = articles2
+articles2 = categories2
+
+user3 = articles3
+articles3 = categories3
+
+user4 = articles4
+articles4 = categories4
+
+user5 = articles5
+articles5 = categories5
+
+user5 = articles5
+articles5 = categories5
+
+user6 = articles6
+articles6 = categories6
+
+user7 = articles7
+articles7 = categories7
+
+user8 = articles8
+articles8 = categories8
+
+user9 = articles9
+articles9 = categories9
+
+user10 = articles10
+articles10 = categories10
